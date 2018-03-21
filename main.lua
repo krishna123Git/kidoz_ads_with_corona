c075437e495e74c32b5755e30857037b743652ab
krishna123Git <36838054+krishna123Git@users.noreply.github.com>
2018-03-21 22:11:59 +0530

    First commit

---
+++ main.lua
@@ -0,0 +1,60 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local widget = require( "widget" )
+
+local kidoz  = require( "plugin.kidoz" )
+
+local publisherID = "13417"
+local securityToken = "KjP9fpLbAGzNrlu58YA2A69ssHwYpnbD"
+
+_H = display.contentHeight
+_W = display.contentWidth
+
+-- We recommend hiding the status bar so our
+-- full-screen videos are not obstructed
+display.setStatusBar( display.HiddenStatusBar )
+display.setDefault( "background", 1 )
+
+local function handleDefaultAdPlay( event )
+    if ( "ended" == event.phase ) then
+		kidoz.show( "interstitial" )
+    end
+end
+local defaultAdButton = widget.newButton {
+    defaultFile = "images/sfSky.jpg",
+    onRelease = handleDefaultAdPlay,
+    -- While ads are caching, our buttons are disabled
+    -- and inform the user to please wait
+    label = "Please wait..",
+    labelColor = { default={ 255, 255, 255, 1.0 }, over={ 0, 0, 0, 0.5 } },
+    fontSize = 36,
+    isEnabled = false,
+    x = _W / 2,
+    y = _H / 6 + 35
+}
+defaultAdButton:setLabel( "Please wait..." )
+defaultAdButton:setEnabled( false )
+
+
+
+local function adListener( event )
+ 
+    if ( event.phase == "init" ) then  -- Successful initialization
+        print( event.provider )
+		kidoz.load( "interstitial" )
+	elseif( event.phase == "loaded" ) then
+		defaultAdButton:setLabel( "Play Default Ad" )
+        defaultAdButton:setEnabled( true )
+	elseif( event.phase == "closed" ) then
+		print("pre load ads")
+		kidoz.load( "interstitial" )
+    end
+end
+ 
+-- Initialize the KIDOZ plugin
+kidoz.init( adListener, { publisherID=publisherID, securityToken=securityToken } )
